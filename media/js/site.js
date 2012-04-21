25287887bda48f0a553cf426bda7bbf9ccf5a657
Or Zubalsky <juviley@gmail.com>
2012-04-21 18:35:23 -0400

    separated js logic to support continuous video playing

--- media/js/site.js
+++ media/js/site.js
@@ -11,36 +11,43 @@
 	     *  These functions are called when the document loads
 	     */
 		this.init = function() 
-		{
-		    this.vimeoControls();
+		{		    
+		    this.initVimeo();
             this.videoSelection();
-            this.pageSelection();
-		};
-			
+            this.pageSelection();		    
+		};	
 			
 		/*
-		 * 
+		 *  Wait for the vimeo player ready event and bind events
 		 */	
-		this.vimeoControls = function() 
+		this.initVimeo = function() 
 		{		    
 		    var self = this;
+            
+            // Listen for the ready event for any vimeo video players on the page
+            var vimeoPlayers = document.querySelectorAll('iframe'), player;
 
-            $('iframe').each(function(){
-                $f(this).addEvent('ready', ready);
-            }           
-
-            function ready(player_id){
-                $f(player_id).addEvent('play', play);
-                $f(player_id).api('play');
-                alert("Ready!!!");
-            }
-            function play(){
-                alert("Playing!!!");
-            }   
-                 
+            for (var i = 0, length = vimeoPlayers.length; i < length; i++) {
+                player = vimeoPlayers[i];
+                $f(player).addEvent('ready', self.vimeoReady);
+            } 
 		};
-			
-			
+		
+		/*
+		 *  When the vimeo player is ready, start playing and bind finish event
+		 */		
+		this.vimeoReady = function(player_id) 
+		{
+		    var self = this;
+            $f(player_id).addEvent('finish', finish);                
+            $f(player_id).api('play');		
+            
+            function finish()
+            {
+                self.loadVideo('/video/1/');
+            }                
+		};
+
 		/*
 		 *  Clicking on a thumbnail displays the target video
 		 */	
@@ -51,33 +58,30 @@
 		    $('#allVideos > a').live('click', function(e) 
 		    {
 		        e.preventDefault();
-		        self.displayVideo(this);
+                self.loadVideo($(this).attr('href'));
 		    });
 		};
-		
 
-		/*
-		 *  Display selected video
-		 *  @param data received from ajax call
-		 */
-		 this.displayVideo = function(anchor)
-		 {
+ 		/*
+ 		 *  Ajax a new video
+ 		 */		
+		this.loadVideo = function(url)
+		{            
+		    var self = this;
 		    var container = $('#mainVideo');
-		    
- 			lib.ajax($(anchor).attr('href'), '', 'json', container, function(data) 
- 			{
- 			    var code    = data[0].fields.code;
-    			var author  = data[0].fields.author;
-    			var page    = data[0].fields.page;
-    			var src     = 'http://player.vimeo.com/video/' + code + '?title=0&amp;byline=0&amp;portrait=0';
-    			var html = '<iframe width="400" height="300" frameborder="0" allowfullscreen="" mozallowfullscreen="" webkitallowfullscreen="" src="http://player.vimeo.com/video/' + code + '?title=0&amp;byline=0&amp;portrait=0"></iframe>';
-    			
-                $('#vimeoFrame').attr('src', src);
-    			
- 			});
-		 };
-		 
-		 
+ 			lib.ajax(url, '', 'json', container, function(data) { 
+ 			    self.editIframeSrc(data[0].fields.code); 
+ 			});		    
+		}
+		
+ 		/*
+ 		 *  Change the source of the Vimeo iFrame player 
+ 		 */		
+		this.editIframeSrc = function(vimeoCode)
+		{
+		    var src     = 'http://player.vimeo.com/video/' + vimeoCode + '?api=1&amp;player_id=vimeoFrame';
+            $('#vimeoFrame').attr('src', src);		    
+		}
 		 
  		/*
  		 *  Clicking on a nav anchor displays the page
@@ -89,23 +93,18 @@
  		    $('#container > a').live('click', function(e) 
  		    {
  		        e.preventDefault();
- 		        self.loadView(this);
- 		    });
- 		};
- 		
- 				 
- 		/*
- 		 *  Populate main content via ajax according to page requested  
- 		 */
- 		 this.loadView = function(anchor)
- 		 {
- 		    var container = $('#main');
+     		   
+     		    var container = $('#main');
 
-  			lib.ajax($(anchor).attr('href'), '', 'html', container, function(data) 
-  			{
-     			$(container).empty().html(data);
-  			});
- 		 };		 
+      			lib.ajax(
+      			    $(this).attr('href'), 
+      			    '', 
+      			    'html', 
+      			    container, 
+      			    function(data) { $(container).empty().html(data); }
+      			); 		    
+      		});
+ 		};
 	};
 })(jQuery);
 
@@ -115,5 +114,5 @@
  */
 $(document).ready(function()
 {   
-	site.init();
+	site.init();	
 });
