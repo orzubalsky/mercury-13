1961c5e21e5763d0efc97ec4d0f942221149bfd4
Or Zubalsky <juviley@gmail.com>
2012-05-02 15:32:31 -0400

    added pattern for next video ajax call

--- videos/urls.py
+++ videos/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'videos.views.index'),
+    url(r'^(?P<video_id>\d+)/next/$', 'videos.views.next'),                      
     url(r'^(?P<video_id>\d+)/$', 'videos.views.detail'),    
-    url(r'^add/$', 'videos.views.add'),        
+    url(r'^add/$', 'videos.views.add'),  
 )
