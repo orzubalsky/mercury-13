64fa72184897890551b928b216f511949771391d
Or Zubalsky <juviley@gmail.com>
2012-06-05 01:44:07 +0300

    added proper extra fields and validation

--- do-you-consider-yourself-a-candidate-for-space-travel/videos/forms.py
+++ do-you-consider-yourself-a-candidate-for-space-travel/videos/forms.py
@@ -6,3 +6,6 @@ class VideoForm (ModelForm):
     class Meta: 
         model = Video
         fields = ['author', 'message']
+    
+    page_number = forms.ModelChoiceField(queryset=Page.objects.all(), widget=forms.HiddenInput, error_messages={'required': 'please select a page'})
+    filename    = forms.CharField(widget=forms.HiddenInput, error_messages={'required': 'Please upload a video file'})
